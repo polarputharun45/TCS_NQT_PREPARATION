53189df7c2505413e11ea2e58843d22a3de08f5f
POLARPU THARUN <tharunsurya1729@gmail.com>
2026-06-13 23:28:21 +0530

    update

---
+++ Day9/IntegerToBinary.java
@@ -0,0 +1,16 @@
+package Day9;
+import java.util.*;
+public class IntegerToBinary {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+
+        int n=sc.nextInt();
+
+        System.out.println(Integer.toBinaryString(n));
+
+
+
+
+    }
+    
+}
