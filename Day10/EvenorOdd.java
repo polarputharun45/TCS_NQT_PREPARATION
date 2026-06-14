28911c8d29405881ff54a8afa58be35163071501
POLARPU THARUN <tharunsurya1729@gmail.com>
2026-06-14 22:24:36 +0530

    update

---
+++ Day10/EvenorOdd.java
@@ -0,0 +1,11 @@
+class EvenorOdd{
+    public static void main(String[]args){
+        int n= 10;
+        if((n&1)==0){
+            System.out.println("even");
+        }
+        else{
+            System.out.println("odd");
+        }
+    }
+}
